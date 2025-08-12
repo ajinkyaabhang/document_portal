752b9b4c34bd209b1e43b0590e14cbb3e0c191bb
ajinkyaabhang <ajinkyaabhang@gmail.com>
2025-08-12 21:32:33 +0200

    new files have been added

--- archive/src/document_analyzer/data_analysis.py
+++ archive/src/document_analyzer/data_analysis.py
@@ -55,5 +55,3 @@ class DocumentAnalyzer:
         except Exception as e:
             self.log.error("Metadata analysis failed", error=str(e))
             raise DocumentPortalException("Metadata extraction failed",sys)
-        
-    
